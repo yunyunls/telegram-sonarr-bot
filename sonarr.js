97f66d22fdf7e5b0156d504c576e5d788bd7394f
Ethras <Vladimir@VPC-MBASSI.cinformatique.local>
2016-11-23 13:12:52 +0100

    merge with master

--- sonarr.js
+++ sonarr.js
@@ -130,25 +130,25 @@ bot.on('message', function(msg) {
   var currentState = cache.get('state' + user.id);
 
   if (currentState === state.admin.REVOKE) {
-      verifyUser(user.id);
-      return handleRevokeUser(user.id, message);
+    verifyUser(user.id);
+    return handleRevokeUser(user.id, message);
   }
-  
+
   if (currentState === state.admin.REVOKE_CONFIRM) {
-      verifyUser(user.id);
-      return handleRevokeUserConfirm(user.id, message);
+    verifyUser(user.id);
+    return handleRevokeUserConfirm(user.id, message);
   }
-  
+
   if (currentState === state.admin.UNREVOKE) {
-      verifyUser(user.id);
-      return handleUnRevokeUser(user.id, message);
+    verifyUser(user.id);
+    return handleUnRevokeUser(user.id, message);
   }
-  
+
   if (currentState === state.admin.UNREVOKE_CONFIRM) {
-      verifyUser(user.id);
-      return handleUnRevokeUserConfirm(user.id, message);
+    verifyUser(user.id);
+    return handleUnRevokeUserConfirm(user.id, message);
   }
-  
+
   if (currentState === state.sonarr.CONFIRM) {
     verifyUser(user.id);
     logger.info(i18n.__('botChatQuerySeriesConfirm', user.id, message));
